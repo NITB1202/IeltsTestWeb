882b10d2125f45e1c896d2564cadc4bcd0e49f2f
Test_repo <22520992@gm.uit.edu.vn>
2024-12-27 12:30:48 +0700

    Add upload full test API

--- IeltsTestWeb/Controllers/TestController.cs
+++ IeltsTestWeb/Controllers/TestController.cs
@@ -4,6 +4,10 @@ using IeltsTestWeb.ResponseModels;
 using IeltsTestWeb.Utils;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using NPOI.XWPF.UserModel;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace IeltsTestWeb.Controllers
 {
@@ -186,6 +190,139 @@ namespace IeltsTestWeb.Controllers
                 return Ok("The test is valid");
 
             return BadRequest("The test should consist of 40 questions. Curent questions: " + questionNum);
+        }
+
+        /// <summary>
+        /// Upload full test file.
+        /// </summary>
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadReadingTest(IFormFile file)
+        {
+            if (file == null || file.Length == 0 || Path.GetExtension(file.FileName) != ".docx")
+                return BadRequest("Invalid file");
+
+            string content;
+            using (var memoryStream = new MemoryStream())
+            {
+                await file.CopyToAsync(memoryStream);
+
+                memoryStream.Seek(0, SeekOrigin.Begin);
+                try
+                {
+                    using (var doc = new XWPFDocument(memoryStream))
+                    {
+                        StringBuilder docContent = new StringBuilder();
+                        foreach (var paragraph in doc.Paragraphs)
+                        {
+                            docContent.Append(paragraph.Text);
+                        }
+
+                        content = docContent.ToString();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest($"Error reading DOCX file: {ex.Message}");
+                }
+            }
+
+            var jsonObjects = ParseJsonFromDoc(content);
+
+            var test = new Test();
+            var sections = new List<ReadingSection>();
+            var questionLists = new List<QuestionList>();
+            var questions = new List<Question>();
+
+            foreach (var jsonObject in jsonObjects)
+            {
+                string key = jsonObject.Key;
+                string json = jsonObject.Value;
+
+                if (key.StartsWith("test"))
+                {
+                    var readTest = JsonConvert.DeserializeObject<Test>(json);
+                    if (readTest != null)
+                    {
+                        database.Tests.Add(readTest);
+                        await database.SaveChangesAsync();
+
+                        test = readTest;
+                    }
+                }
+                else if (key.StartsWith("section"))
+                {
+                    var section = JsonConvert.DeserializeObject<ReadingSection>(json);
+                    if(section != null)
+                    {
+                        section.TestId = test.TestId;
+                        database.ReadingSections.Add(section);
+                        await database.SaveChangesAsync();
+
+                        sections.Add(section);
+                    }
+                }
+                else if (key.StartsWith("questionList"))
+                {
+                    var questionList = JsonConvert.DeserializeObject<QuestionList>(json);
+                    if (questionList != null)
+                    {
+                        questionList.Rsections.Add(sections.Last());
+                        database.QuestionLists.Add(questionList);
+                        await database.SaveChangesAsync();
+
+                        questionLists.Add(questionList);
+                    }
+                }
+                else if (key.StartsWith("matchingQuestionList"))
+                {
+                    var matchingQuestionList = JsonConvert.DeserializeObject<MatchQuestionList>(json);
+                    if (matchingQuestionList != null)
+                    {
+                        matchingQuestionList.QlistId = questionLists.Last().QlistId;
+                        database.MatchQuestionLists.Add(matchingQuestionList);
+                        await database.SaveChangesAsync();
+                    }
+                }
+                else if (key.StartsWith("question"))
+                {
+                    var question = JsonConvert.DeserializeObject<Question>(json);
+                    if(question != null)
+                    {
+                        question.QlistId = questionLists.Last().QlistId;
+                        database.Questions.Add(question);
+                        await database.SaveChangesAsync();
+
+                        questions.Add(question);
+                    }
+                }
+                else if (key.StartsWith("explanation"))
+                {
+                    var explanation = JsonConvert.DeserializeObject<Explanation>(json);
+                    if (explanation != null)
+                    {
+                        explanation.QuestionId = questions.Last().QuestionId;
+                        database.Explanations.Add(explanation);
+                        await database.SaveChangesAsync();
+                    }
+                }
+            }
+
+            return Ok(jsonObjects);
+        }
+
+        private Dictionary<string, string> ParseJsonFromDoc(string content)
+        {
+            var result = new Dictionary<string, string>();
+            var matches = Regex.Matches(content, @"“(\w+)”:\s*({.*?})", RegexOptions.Singleline);
+
+            foreach (Match match in matches)
+            {
+                string key = match.Groups[1].Value;
+                string json = match.Groups[2].Value.Replace("“", "\"").Replace("”", "\"");
+                result[key] = json;
+            }
+
+            return result;
         }
     }
 }
